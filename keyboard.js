a5263b3f0a4e01d3aae982385bbff2faec56c160
Michael Kohl <citizen428@gmail.com>
2011-10-27 13:12:48 +0200

    Remove debugging

--- keyboard.js
+++ keyboard.js
@@ -1,10 +1,7 @@
 function keyListener(e) {
-console.log(e.altKey);
-console.log(e.shiftKey);
-console.log(e.keyCode);
-    if (e.altKey && e.shiftKey && e.keyCode == 83) {
-        chrome.extension.sendRequest({name: "openPopUp"});
-    }
+  if (e.altKey && e.shiftKey && e.keyCode == 83) {
+      chrome.extension.sendRequest({name: "openPopUp"});
+  }
 };
 
 window.addEventListener("keyup", keyListener, false);
